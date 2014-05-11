16e0838880bf4b0632b156e7dfcc0a378be81074
ka2er <personseb+git@gmail.com>
2014-05-11 12:39:41 +0200

    [NEW] yo angular !

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('quelPrenomApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
